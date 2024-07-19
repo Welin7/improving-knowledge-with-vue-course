9c5d3bcc6df0abb8bdf4cd4afcbd2c6b6f2ba779
Welington Dias <33883532+Welin7@users.noreply.github.com>
2024-07-18 23:22:58 -0300

    class one -using interpolation.

---
+++ vue3-course/src/App.vue
@@ -0,0 +1,15 @@
+<!--SCRIPT-->
+<script setup>
+//variable
+let name = "Carlos Veiga";
+let age = "20";
+
+let numberOne = 100;
+let numberTwo = 200;
+</script>
+
+<!--HTML-->
+<template>
+  <p>{{ "Hi " + name + " you have " + age + " old" }}</p>
+  <p>{{ numberOne+numberTwo }}</p>
+</template>
---
+++ vue3-course/src/main.js
@@ -0,0 +1,8 @@
+//Module createApp
+import { createApp } from 'vue'
+
+//Import file App.vue
+import App from './App.vue'
+
+//Render
+createApp(App).mount('#app')
