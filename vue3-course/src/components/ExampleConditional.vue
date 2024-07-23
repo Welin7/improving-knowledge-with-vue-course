8d71e05f7f8f09d85f74bab261c4a3cf65bcf9de
Welington Dias <33883532+Welin7@users.noreply.github.com>
2024-07-23 00:07:45 -0300

    class eight - conditional v-if, v-else if and v-else

--- vue3-course/src/components/ExampleConditional.vue
+++ vue3-course/src/components/ExampleConditional.vue
@@ -3,9 +3,15 @@
 //variable
 let exibition = true;
 
+let average = 6.5;
+
 </script>
 
 <!--HTML-->
 <template>
     <h1 v-if="exibition"> Helo Conditional VIF!</h1>
+    
+    <p v-if="average >= 6">Aprooved</p>
+    <p v-else-if="average >= 5">Under Examination</p>
+    <p v-else>Disapproved</p>
 </template>
